a3b233e243fcbbcd7999a72ba914218a1832dee8
reinhart2013 <mduduzi.bhebhe1@icloud.com>
2025-06-11 11:24:01 +0100

    Initial Commit

---
+++ I am a programmer./ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I am a programmer.
+//
+//  Created by Mduduzi Bhebhe on 11/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I am a programmer./I_am_a_programmer_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_a_programmer_App.swift
+//  I am a programmer.
+//
+//  Created by Mduduzi Bhebhe on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct I_am_a_programmer_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
